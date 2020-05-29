9018a8567ea9b07be5371e877c113f370c07cf88
Wolverine <Wolverine@DESKTOP-942HOHM>
2020-05-29 13:16:00 +0530

    Initial commit
    Change-Id: Ia9e4bd829f85c45660f7c565c32376364532f5d4

---
+++ eureka-naming-server/src/main/java/com/microservices/eurekanamingserver/EurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.eurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+
+}
